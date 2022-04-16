48f1d6c139cb5b5c43fcac4e87dc076759872676
Suhani220 <102501106+Suhani220@users.noreply.github.com>
2022-04-16 11:32:50 +0530

    Add files via upload

---
+++ P09_Task01_AbstractAndFinalClassInJava.java
@@ -0,0 +1,58 @@
+package Unit_02;
+
+abstract class Bike {
+	
+	int a;
+	
+	Bike(){	
+		System.out.println("Inside Bike Constructor : " + a);
+	}
+	
+	abstract void run();
+	
+	void display() {
+		System.out.println("This is display method.");
+	}
+	
+}
+
+class Honda4 extends Bike{
+
+	@Override
+	void run() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
+
+public class P09_Task01_AbstractAndFinalClassInJava {
+	public static void main(String[] args) {
+		
+		Bike obj = new Honda4();
+		obj.display();
+		
+		
+	}
+}
+
+final class Super{
+	
+	public int data = 30;
+	void display() {
+		System.out.println(data);
+	}
+	
+}
+
+// Final class cannot be subclassed.
+
+/*
+public class sub extends Super{
+	
+	void display2() {
+		display();
+	}
+	
+}
+*/
